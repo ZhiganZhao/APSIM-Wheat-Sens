41d039b74c414404cd98e95929bbf1f3ad915603
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-05-24 20:17:27 +1000

    Update compile scripts

--- _generate-gh-pages.R
+++ _generate-gh-pages.R
@@ -1,8 +1,8 @@
-
-library(rmarkdown)
-unlink('site_libs', recursive = TRUE)
-
-tryCatch(render_site(), error = function(e) {
+tryCatch({
+    library(rmarkdown)
+    clean_site()
+    render_site()
+}, error = function(e) {
     print(e)
     quit(save = "no", status = 100, runLast = FALSE)
-    })
+})

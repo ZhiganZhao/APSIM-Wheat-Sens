002b50ab8dd9ebfb7cb504fb96fad092a08cfa54
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-05-24 21:29:12 +1000

    Add a template for parameter list.

--- _generate-gh-pages.R
+++ _generate-gh-pages.R
@@ -1,8 +1,37 @@
 library(rmarkdown)
-
+library(yaml)
 tryCatch({
     clean_site()
+    para <- yaml.load_file('_parameter.yml')
+    navbar <- yaml.load_file('_navbar.yml')
+    
+    navbar_new <- navbar
+    navbar_new$left <- c(navbar_new$left, para)
+    
+    writeLines(as.yaml(navbar_new), '_navbar.yml')
+    
+    for (i in seq(along = para)) {
+        # i <- 1
+        para_i <- para[[i]]$menu
+        for (j in seq(along = para_i)) {
+            # j <- 1
+            para_j <- para_i[[j]]
+            file_name <- paste0(gsub('(.*)\\..*', '\\1', para_j$href), '.Rmd')
+            template <- readLines('_template.Rmd')
+            # Change template
+            rmd_new <- template
+            writeLines(rmd_new, file_name)
+            render(
+                file_name 
+                , params = list(
+                    para = para_j)
+            )
+            file.remove(file_name)
+        }
+    }
+    
     render_site()
+    writeLines(as.yaml(navbar, indent = 4), '_navbar.yml')
 }, error = function(e) {
     print(e)
     quit(save = "no", status = 100, runLast = FALSE)

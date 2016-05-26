8b2a22e9c83ce9c3197f6f43d38f0b3572b01c85
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-05-26 18:33:59 +1000

    Test me

--- _build.sh
+++ _build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+# configure your name and email if you have not done so
+git config --global user.email "zheng.bangyou@gmail.com"
+git config --global user.name "Bangyou Zheng"
+git clone --branch=gh-pages \
+  https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git \
+  site-output
+cd site-output
+
+git pull --no-edit origin master
+
+
 if [[ $(git diff --name-only | grep "sensitivity|_parameter") ]]; then
     sudo apt-get install -y mono-complete
 else
@@ -9,14 +20,7 @@ fi
 
 sudo apt-get install -y mono-complete
 
-# configure your name and email if you have not done so
-git config --global user.email "zheng.bangyou@gmail.com"
-git config --global user.name "Bangyou Zheng"
-git clone --branch=gh-pages \
-  https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git \
-  site-output
-cd site-output
-git pull --no-edit origin master
+
 Rscript _generate-gh-pages.R
 git add *
 git commit -m "Update the website"
